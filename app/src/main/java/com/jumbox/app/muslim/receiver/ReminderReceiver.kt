b18267d553471ec5e91d07bb59d976d4a281a8c8
Jumadi <jumadi@firstpayment.co.id>
2021-11-15 20:51:18 +0700

    add background compass dark

--- app/src/main/java/com/jumbox/app/muslim/receiver/ReminderReceiver.kt
+++ app/src/main/java/com/jumbox/app/muslim/receiver/ReminderReceiver.kt
@@ -5,6 +5,7 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -41,7 +42,7 @@ class ReminderReceiver : DaggerBroadcastReceiver() {
         const val CHANNEL_NAME = "Notification Adzan"
 
         private const val ALARM_ID = 1010
-        const val ACTION_REMINDER = "com.jumbox.app.muslim.ACTION_REMINDER"
+        const val ACTION_REMINDER = "com.jumbox.app.islamic.ACTION_REMINDER"
 
         private const val EXTRA_PRAYER_TIME = "extra_time"
         private const val EXTRA_PRAYER_ID = "extra_id"
@@ -171,7 +172,9 @@ class ReminderReceiver : DaggerBroadcastReceiver() {
                                     preference.alarmTimeOut.toString(),
                                     context.getString(it.name.nameResource(R.string::class.java))
                                 )
-                                notify(context, title, body, prayer.time)
+                                notify(context, title, body, prayer.time,
+                                    Uri.parse("android.resource://"
+                                            + context.packageName + "/" + if (prayer.name == "fajr") R.raw.fajr else R.raw.normal))
                                 nextReminder(context, time)
                             }
                         }
@@ -202,7 +205,9 @@ class ReminderReceiver : DaggerBroadcastReceiver() {
                     )
                 )
             }
-            notify(context, title, body, prayer.time)
+            notify(context, title, body, prayer.time,
+                Uri.parse("android.resource://"
+                        + context.packageName + "/" + if (prayer.name == "fajr") R.raw.fajr else R.raw.normal))
             nextReminder(context, prayer.time)
         }
     }
@@ -211,20 +216,21 @@ class ReminderReceiver : DaggerBroadcastReceiver() {
         context: Context,
         title: String,
         msg: String,
-        id: Long
+        id: Long,
+        sound: Uri,
     ) {
         val managerCompat = NotificationManagerCompat.from(context)
         val pendingIntent = PendingIntent.getActivity(
-                        context, 0,
-                        Intent(context, PrayerTimeActivity::class.java)
-                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK),
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                )
+            context, 0,
+            Intent(context, PrayerTimeActivity::class.java)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK),
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(title)
-                .setContentText(msg)
-            .setStyle(NotificationCompat.BigTextStyle().bigText(msg))
+            .setContentText(msg)
+            .setStyle(NotificationCompat.BigTextStyle().bigText(msg)).setSound(sound)
             .setAutoCancel(true).setContentIntent(pendingIntent)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
